88ad4e0dcfa4e224a2ec2963b4af74d3ee43cd5b
sabuthess <dilan.developer22@gmail.com>
2026-03-12 22:45:31 -0500

    Remove register page

--- src/app/register/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-	title: "Registrarse | Esfrar",
-	description: "Crear una nueva cuenta en Esfrar",
-};
-
-export default function RegiserLayout({ children }) {
-	return <>{children}</>;
-}
