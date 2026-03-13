63ebc97c7f08f46d14bdb3a5d489bfced8f0bcae
sabuthess <dilan.developer22@gmail.com>
2026-03-13 05:48:45 -0500

    Add new styels

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,22 +1,34 @@
-import { Footer } from "@/components/Footer/Footer";
-import { Header } from "@/components/Header/Header";
+import { Footer } from "@/components/ui/Footer";
+import { Header } from "@/components/ui/Header";
 import Link from "next/link";
 
 export default function NotFound() {
 	return (
 		<>
-			<Header />
-			<div className="h-screen flex flex-col justify-center items-center gap-4">
-				<div className="absolute top-0 -z-10 h-full w-full bg-white">
-					<div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(34,197,94,0.5)] opacity-50 blur-[80px]"></div>
-				</div>
+			<div className="absolute inset-0 -z-10">
+				<div
+					className="absolute inset-0"
+					style={{
+						backgroundImage:
+							"linear-gradient(to right, #4f4f4f2e 1px, transparent 1px), linear-gradient(to bottom, #8080800a 1px, transparent 1px)",
+						backgroundSize: "14px 24px",
+					}}
+				/>
 
-				<p className=" font-bold">404</p>
-				<h2 className="font-bold text-5xl">Page not found</h2>
-
-				<p className="text-neutral-600">Sorry, we couldn’t find the page you’re looking for.</p>
-				<Link className="font-bold" href='/'>← Back to home</Link>
+				<div className="absolute top-[20%] right-[30%] h-[500px] w-[500px] rounded-full bg-[rgba(34,197,94,0.5)] blur-[80px] opacity-50" />
 			</div>
+
+			<Header />
+
+			<div className="relative z-10 h-screen flex flex-col justify-center items-center gap-4 text-center text-white">
+				<p className="font-bold text-7xl">404</p>
+				<h2 className="font-bold text-5xl">Page not found</h2>
+				<p>Sorry, we couldn’t find the page you’re looking for.</p>
+				<Link href="/" className="font-bold text-teal-500 hover:underline">
+					← Back to home
+				</Link>
+			</div>
+
 			<Footer />
 		</>
 	);
