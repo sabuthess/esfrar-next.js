fc8e09221cedf65100011f57f02c13bdf69b800a
sabuthess <dilan.developer22@gmail.com>
2026-03-12 22:45:12 -0500

    Remove login page

--- src/app/login/layout.js
+++
@@ -1,9 +0,0 @@
-export const metadata = {
-	title: "Iniciar Sesión | Esfrar",
-	description: "Login de Esfrar",
-};
-
-export default function LoginLayout({ children }) {
-	// Solo devuelves el children, no vuelvas a poner <html>, <body>
-	return <>{children}</>;
-}
