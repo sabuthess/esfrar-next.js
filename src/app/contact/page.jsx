c8c6f459e543f769fcf3eae19e1a668b5a9df2c5
sabuthess <dilan.developer22@gmail.com>
2026-03-13 05:42:46 -0500

    Add new styles

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,5 @@
-import { Footer } from "@/components/Footer/Footer";
-import { Header } from "@/components/Header/Header";
+import { Footer } from "@/components/ui/Footer";
+import { Header } from "@/components/ui/Header";
 
 export default function Contact() {
     return (
@@ -11,36 +11,36 @@ export default function Contact() {
             </main> */}
 
             <div className="relative ">
-                <div class="absolute top-0 -z-10 h-full w-full bg-white"><div class="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(109,244,199,0.5)] opacity-50 blur-[80px]"></div></div>
+                <div class="absolute top-0 -z-10 h-full w-full "><div class="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(109,244,199,0.5)] opacity-50 blur-[80px]"></div></div>
                 <div className="absolute inset-0">
                     <div className="absolute inset-y-0 left-0 w-1/2 " />
                 </div>
                 <div className="relative max-w-7xl mx-auto lg:grid lg:grid-cols-5 ">
                     <div className=" py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
                         <div className="max-w-lg mx-auto">
-                            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">Get in touch</h2>
+                            <h2 className="text-2xl font-extrabold tracking-tight text-white sm:text-3xl">Get in touch</h2>
                             <p className="mt-3 text-lg leading-6 text-gray-500">
                                 Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus
                                 arcu.
                             </p>
-                            <div className="flex flex-col gap-2 my-10">
+                            <div className="flex flex-col gap-2 my-10 text-gray-300">
 
                                 <div className="flex gap-4 items-center">
                                     <hr className="border h-10 border-indigo-500" />
                                     <p>Quito, Ecudor</p>
                                 </div>
                                 <div className="flex gap-4 items-center">
-                                    <hr className="border h-10 border-black" />
+                                    <hr className="border h-10 border-white" />
                                     <p>Av. y Santa Teresa</p>
                                 </div>
                                 <div className="flex gap-4 items-center">
-                                    <hr className="border h-10 border-black" />
+                                    <hr className="border h-10 border-white" />
                                     <p>Pomasqui</p>
                                 </div>
                             </div>
-                            <p className="mt-6 text-base text-gray-500">
+                            <p className="mt-6 text-base text-white">
                                 Looking for careers?{' '}
-                                <a href="#" className="font-medium text-gray-700 underline">
+                                <a href="#" className="font-medium text-gray-600 underline">
                                     View all job openings
                                 </a>
                                 .
@@ -59,7 +59,7 @@ export default function Contact() {
                                         name="full-name"
                                         id="full-name"
                                         autoComplete="name"
-                                        className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                                        className="block w-full shadow-sm py-3 px-4 bg-white/10 text-white border-none outline-none rounded-md"
                                         placeholder="Full name"
                                     />
                                 </div>
@@ -72,7 +72,7 @@ export default function Contact() {
                                         name="email"
                                         type="email"
                                         autoComplete="email"
-                                        className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                                        className="block w-full shadow-sm py-3 px-4 bg-white/10 text-white border-none outline-none rounded-md"
                                         placeholder="Email"
                                     />
                                 </div>
@@ -85,7 +85,7 @@ export default function Contact() {
                                         name="phone"
                                         id="phone"
                                         autoComplete="tel"
-                                        className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                                        className="block w-full shadow-sm py-3 px-4 bg-white/10 text-white border-none outline-none rounded-md"
                                         placeholder="Phone"
                                     />
                                 </div>
@@ -97,7 +97,7 @@ export default function Contact() {
                                         id="message"
                                         name="message"
                                         rows={4}
-                                        className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border border-gray-300 rounded-md"
+                                        className="block w-full shadow-sm py-3 px-4 bg-white/10 text-white border-none outline-none rounded-md"
                                         placeholder="Message"
                                         defaultValue={''}
                                     />
@@ -105,7 +105,7 @@ export default function Contact() {
                                 <div>
                                     <button
                                         type="submit"
-                                        className="inline-flex justify-center py-3 px-6 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                        className="inline-flex justify-center py-3 px-10 border  shadow-sm text-base font-medium rounded-md text-white border-teal-400 hover:bg-teal-600/40 focus:outline-none focus:ring-2 focus:ring-offset-2 "
                                     >
                                         Submit
                                     </button>
